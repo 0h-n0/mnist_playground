b0565befd915127089a913fa710ed7c9608378ce
0h-n0 <kbu94984@gmail.com>
2017-09-13 00:44:26 +0900

    MOD: add sacred.

--- mxnet/mnist_blstm.py
+++ mxnet/mnist_blstm.py
@@ -1,34 +1,19 @@
 #!/usr/bin/env python3
 import struct
+
 from pathlib import Path
 import urllib.request
 
+from sacred import Experiment
+from sacred.observers import FileStorageObserver
 from sklearn.datasets import fetch_mldata
 import numpy as np
 import mxnet as mx
-    
 
-path_to_data = '../data/mnist'
-p = Path(path_to_data)
-
-mnist = fetch_mldata('MNIST original', data_home=p.resolve())
+ex = Experiment('mnist_blstm')
+ex.observers.append(FileStorageObserver.create('results'))
 
 
-print(mnist.target[0])
-print(len(mnist.target))
-
-np.random.seed(1)
-random_seq = np.arange(70000)
-num_train = 60000
-np.random.shuffle(random_seq)
-
-## shuffle data
-train_data = mnist.data[random_seq[:num_train]]
-train_labels = mnist.target[random_seq[:num_train]]
-test_data = mnist.data[random_seq[num_train:]]
-test_labels = mnist.target[random_seq[num_train:]]
-
-### 
 def nn(data):
     flatten = mx.sym.flatten(data=data)
     fc1 = mx.sym.FullyConnected(data=flatten, num_hidden=128)
@@ -38,23 +23,78 @@ def nn(data):
     out = mx.sym.Softmax(data=act2, name='softmax')
     return out
 
-batch = 10
-epoch = 2
-    
-data = mx.sym.var('data')
-model = mx.mod.Module(symbol=nn(data), data_names=['data'],
-                      label_names=['softmax_label'], context=mx.gpu(0))
+@ex.config
+def cfg():
+    path_to_data = '../data/mnist' # set data path.
+    path_to_results = './results'  # set results path.
+    num_train = 60000              # set the number of train samples.
+    batch = 10 # set batch size
+    epoch = 2 # set epoch
+    context = 'cpu' # set background archtecture
+    ngpu = 1 # set the number of gpu
+    model = 'nn' # set the name of model.
+    background = 'mnxet' # set the name of deep learning library
 
-model.bind(data_shapes=[('data', (batch, 784))],
+@ex.capture
+def show_cfg(_log, _seed, path_to_data, path_to_results,
+             num_train, batch, epoch, context, ngpu, model, background):
+    _log.info('path: {}'.format(path_to_data))
+    _log.info('path: {}'.format(path_to_results))    
+    _log.info('random seed: {}'.format(_seed))
+    _log.info('batch: {}'.format(batch))
+    _log.info('epoch: {}'.format(epoch))
+    _log.info('context: {}'.format(context))                
+    _log.info('ngpu: {}'.format(ngpu))
+    _log.info('model: {}'.format(model))
+    _log.info('background: {}'.format(background))        
+
+    
+@ex.capture
+def run(_log, _seed, path_to_data, path_to_results, num_train, batch, epoch,
+        context, ngpu):
+    
+    _log.info('Start train.')
+
+    _log.info('Downloading MNIST DATA.')
+    p = Path(path_to_data)
+    mnist = fetch_mldata('MNIST original', data_home=p.resolve())
+    _log.info('Completed downloading MNIST DATA.')
+
+    np.random.seed(_seed)
+    random_seq = np.arange(70000)
+    num_train = 60000
+    np.random.shuffle(random_seq)
+
+    ## shuffle data
+    train_data = mnist.data[random_seq[:num_train]]
+    train_labels = mnist.target[random_seq[:num_train]]
+    test_data = mnist.data[random_seq[num_train:]]
+    test_labels = mnist.target[random_seq[num_train:]]
+    ### 
+    
+    data = mx.sym.var('data')
+    model = mx.mod.Module(symbol=nn(data), data_names=['data'],
+                          label_names=['softmax_label'], context=mx.cpu())
+
+    model.bind(data_shapes=[('data', (batch, 784))],
            label_shapes=[('softmax_label', (batch, 10))])
 
-model.init_params(initializer=mx.init.Xavier(magnitude=2.))
-model.init_optimizer(optimizer='sgd', optimizer_params=(('learning_rate', 0.1), ))
-metric = mx.metric.create('acc')
+    model.init_params(initializer=mx.init.Xavier(magnitude=2.))
+    model.init_optimizer(optimizer='sgd', optimizer_params=(('learning_rate', 0.1), ))
+    metric = mx.metric.create('acc')
+
+    for i in range(epoch):
+        for j in range(0, num_train - batch, batch):
+            x = train_data[j:j+batch]
+            y = train_labels[j:j+batch]
+            model(x)
+            
+@ex.automain
+def main():
+    show_cfg()
+    run()
 
 
-for i in range(epoch):
-    for j in range(0, num_train, batch):
-        print(train_data[j])
-        #data, targets = generator(batch, labelsize, dtype='l')
-        #_list = []
+
+
+
